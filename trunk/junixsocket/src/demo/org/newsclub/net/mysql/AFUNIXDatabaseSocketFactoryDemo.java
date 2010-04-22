aed2a7181a2ffcc6aaa80b84bb2a8cc4ee994279
ck@newsclub.de <ck@newsclub.de@acdef0e8-93f4-11de-9897-6f94e350a523>
2010-04-22 12:04:59 +0000

    Included the mysql subproject into the build process. Also creates a separate binary.

---
+++ trunk/junixsocket/src/demo/org/newsclub/net/mysql/AFUNIXDatabaseSocketFactoryDemo.java
@@ -0,0 +1,56 @@
+/**
+ * junixsocket
+ *
+ * Copyright (c) 2009 NewsClub, Christian Kohlschütter
+ *
+ * The author licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.newsclub.net.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.Properties;
+
+import org.newsclub.net.mysql.AFUNIXDatabaseSocketFactory;
+
+/**
+ * Demonstrates how to connect to a local MySQL server
+ * 
+ * @author Christian Kohlschuetter
+ */
+public class AFUNIXDatabaseSocketFactoryDemo {
+	public static void main(String[] args) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver").newInstance();
+		Properties props = new Properties();
+		props.put("user", "test");
+		props.put("password", "test");
+		props.put("socketFactory", AFUNIXDatabaseSocketFactory.class.getName());
+		props.put("junixsocket.file", "/var/lib/mysql.sock");
+
+		// SHOW DATABASES three times
+		for (int i = 0; i < 3; i++) {
+			Connection conn = DriverManager.getConnection("jdbc:mysql://",
+					props);
+			System.out.println(conn);
+
+			ResultSet rs = conn.createStatement()
+					.executeQuery("SHOW DATABASES");
+			while (rs.next()) {
+				System.out.println(rs.getString(1));
+			}
+			rs.close();
+			conn.close();
+		}
+	}
+}
---
+++ trunk/junixsocket/src/mysql/com/mysql/jdbc/SocketFactory.java
@@ -0,0 +1,14 @@
+package com.mysql.jdbc;
+
+// JUST TO KEEP THE COMPILER HAPPY
+// NOT TO BE INCLUDED IN FINAL PRODUCT BINARY
+public interface SocketFactory {
+//// Method descriptor #4 ()Ljava/net/Socket;
+  public abstract java.net.Socket afterHandshake() throws java.net.SocketException, java.io.IOException;
+  
+  // Method descriptor #4 ()Ljava/net/Socket;
+  public abstract java.net.Socket beforeHandshake() throws java.net.SocketException, java.io.IOException;
+  
+  // Method descriptor #10 (Ljava/lang/String;ILjava/util/Properties;)Ljava/net/Socket;
+  public abstract java.net.Socket connect(java.lang.String arg0, int arg1, java.util.Properties arg2) throws java.net.SocketException, java.io.IOException;
+}
