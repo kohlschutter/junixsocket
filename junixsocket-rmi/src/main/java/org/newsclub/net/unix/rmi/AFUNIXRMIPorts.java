f22d1ad11957aee777de5658869b0fa4dfc2b062
Christian Kohlschütter <christian@kohlschutter.com>
2020-01-16 20:50:32 -0800

    Update license headers

--- junixsocket-rmi/src/main/java/org/newsclub/net/unix/rmi/AFUNIXRMIPorts.java
+++ junixsocket-rmi/src/main/java/org/newsclub/net/unix/rmi/AFUNIXRMIPorts.java
@@ -1,7 +1,7 @@
 /**
  * junixsocket
  *
- * Copyright 2009-2019 Christian Kohlschütter
+ * Copyright 2009-2020 Christian Kohlschütter
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
