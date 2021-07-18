0e5e4827985fa0356b2e9d9530bf8292feddc9dc
Christian Kohlschütter <christian@kohlschutter.com>
2021-07-18 15:46:58 -0700

    Enable ECJ @NonNull etc. annotations

--- junixsocket-common/src/main/java/module-info.java
+++ junixsocket-common/src/main/java/module-info.java
@@ -4,4 +4,5 @@ module org.newsclub.net.unix {
   requires java.base;
   requires static java.rmi;
   requires static com.kohlschutter.annotations.compiletime;
+  requires transitive static org.eclipse.jdt.annotation;
 }
