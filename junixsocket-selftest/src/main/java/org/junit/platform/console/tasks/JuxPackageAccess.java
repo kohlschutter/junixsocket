cdb818aeef9fe1f0c7d03c93337d4a4de7110a66
Christian Kohlschütter <christian@kohlschutter.com>
2022-06-30 18:51:20 +0200

    junixsocket-selftest: Add javadoc comment

--- junixsocket-selftest/src/main/java/org/junit/platform/console/tasks/JuxPackageAccess.java
+++ junixsocket-selftest/src/main/java/org/junit/platform/console/tasks/JuxPackageAccess.java
@@ -5,6 +5,11 @@ import java.io.PrintWriter;
 import org.junit.platform.console.options.Theme;
 import org.junit.platform.launcher.TestExecutionListener;
 
+/**
+ * Helper class to access {@link TreePrintingListener}.
+ * 
+ * @author Christian Kohlschütter
+ */
 public final class JuxPackageAccess {
   public static TestExecutionListener newTreePrintingListener(PrintWriter out,
       boolean disableAnsiColors, Theme theme) {
