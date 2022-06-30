125f3e56114e65062688c57d52d5dca844d4408a
Christian Kohlschütter <christian@kohlschutter.com>
2022-06-30 18:11:49 +0200

    Cleanup code

--- junixsocket-common/src/main/java/org/newsclub/net/unix/FileDescriptorCast.java
+++ junixsocket-common/src/main/java/org/newsclub/net/unix/FileDescriptorCast.java
@@ -31,7 +31,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.RandomAccess;
 import java.util.Set;
 import java.util.function.Function;
 
