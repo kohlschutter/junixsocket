9d0db23551a6028cca0cc63c34cb270419351a73
Christian Kohlschütter <christian@kohlschutter.com>
2022-10-08 01:21:40 +0200

    Add out-of-box support for GraalVM native-images
    Packages necessary META-INF/native-image JSON configuration files so
    GraalVM knows what to do when it builds a native-image containing
    junixsocket.
    
    What's still a somewhat manual task is to update the JSON files when
    junixsocket-native-graalvm/bin/build-selftest detects configuration
    changes (hopefully that's not a frequent case; it can be detected
    automatically though by running that script).
    
    Add junixsocket-selftest-native-image: Creates a working junixsocket
    selftest as a GraalVM native image.

---
+++ junixsocket-selftest-native-image/src/main/java/org/newsclub/net/unix/selftest/nativeimage/Main.java
@@ -0,0 +1,41 @@
+/*
+ * junixsocket
+ *
+ * Copyright 2009-2022 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.newsclub.net.unix.selftest.nativeimage;
+
+import org.newsclub.net.unix.selftest.Selftest;
+
+/**
+ * Main entry.
+ * 
+ * @author Christian Kohlschütter
+ */
+public final class Main { // NOPMD
+  private Main() {
+    throw new IllegalStateException("No instances");
+  }
+
+  /**
+   * Starts junixsocket-selftest.
+   * 
+   * @param args The args.
+   * @throws Exception on error.
+   */
+  public static void main(String[] args) throws Exception {
+    Selftest.main(args);
+  }
+}
