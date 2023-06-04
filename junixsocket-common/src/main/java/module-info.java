447feb8b23a5618f83bbebb3da13625ee279703a
Christian Kohlschütter <christian@kohlschutter.com>
2023-06-04 13:38:49 +0200

    module-info: Make sure compile time annotation dependencies are transitive

--- junixsocket-common/src/main/java/module-info.java
+++ junixsocket-common/src/main/java/module-info.java
@@ -6,6 +6,6 @@ module org.newsclub.net.unix {
 
   requires java.base;
   requires static java.rmi;
-  requires static com.kohlschutter.annotations.compiletime;
+  requires static transitive com.kohlschutter.annotations.compiletime;
   requires static org.eclipse.jdt.annotation;
 }
