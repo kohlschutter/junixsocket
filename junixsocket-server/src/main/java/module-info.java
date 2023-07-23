e39dabd5ff185feede11a7fe62db0360e0348f70
Christian Kohlschütter <christian@kohlschutter.com>
2023-07-23 13:50:43 +0200

    server: module-info: require static org.eclipse.jdt.annotation
    Previously, we erroneously required "org.eclipse.jdt.annotation" at
    runtime.
    
    Change "requires" to "requires static", so it's only required at compile
    time.
    
    https://github.com/kohlschutter/junixsocket/issues/137

--- junixsocket-server/src/main/java/module-info.java
+++ junixsocket-server/src/main/java/module-info.java
@@ -6,5 +6,5 @@ module org.newsclub.net.unix.server {
 
   requires transitive org.newsclub.net.unix;
   requires static com.kohlschutter.annotations.compiletime;
-  requires org.eclipse.jdt.annotation;
+  requires static org.eclipse.jdt.annotation;
 }
