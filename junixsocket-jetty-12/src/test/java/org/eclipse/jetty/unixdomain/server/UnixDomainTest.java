92c0ec47157059c7eb980f839cbdc3f34e697609
Christian Kohlschütter <christian@kohlschutter.com>
2023-08-13 21:59:08 +0200

    jetty: Add junixsocket-jetty-12 (for testing)
    While the runtime support for jetty is compatible with Jetty 12, unit
    tests currently rely on outdated code that's no longer running with
    Jetty 12.
    
    Add a separate junixsocket-jetty-12 project, with unit tests slightly
    modified to compile and run with Jetty 12.

---
+++ junixsocket-jetty-12/src/test/java/org/eclipse/jetty/unixdomain/server/UnixDomainTest.java
@@ -0,0 +1,356 @@
+/*
+ * junixsocket
+ *
+ * Copyright 2009-2022 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+//
+// ========================================================================
+// Copyright (c) 1995-2022 Mort Bay Consulting Pty Ltd and others.
+//
+// This program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v. 2.0 which is available at
+// https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+// which is available at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+// ========================================================================
+//
+
+package org.eclipse.jetty.unixdomain.server;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.SocketAddress;
+import java.net.SocketException;
+import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.jetty.client.ContentResponse;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpProxy;
+import org.eclipse.jetty.client.ProxyProtocolClientConnectionFactory.V1;
+import org.eclipse.jetty.client.ProxyProtocolClientConnectionFactory.V2;
+import org.eclipse.jetty.client.transport.HttpClientTransportDynamic;
+import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http.HttpStatus;
+import org.eclipse.jetty.http.HttpURI;
+import org.eclipse.jetty.io.ClientConnector;
+import org.eclipse.jetty.server.ConnectionFactory;
+import org.eclipse.jetty.server.ConnectionMetaData;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.ProxyConnectionFactory;
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Response;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.util.Callback;
+import org.eclipse.jetty.util.component.LifeCycle;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.newsclub.net.unix.AFSocketAddress;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
+import org.newsclub.net.unix.jetty.AFSocketClientConnector;
+import org.newsclub.net.unix.jetty.AFSocketServerConnector;
+
+public class UnixDomainTest {
+  private static final Class<?> unixDomainSocketAddressClass = probe();
+
+  private static Class<?> probe() {
+    try {
+      return ClassLoader.getPlatformClassLoader().loadClass("java.net.UnixDomainSocketAddress");
+    } catch (Throwable x) {
+      return null;
+    }
+  }
+
+  private ConnectionFactory[] factories = new ConnectionFactory[] {new HttpConnectionFactory()};
+  private Server server;
+  private Path unixDomainPath;
+
+  @BeforeAll
+  public static void setUp() {
+    System.setProperty("org.slf4j.simpleLogger.log.org.eclipse.jetty", "error");
+  }
+
+  private void start(Handler handler) throws Exception {
+    server = new Server();
+    // UnixDomainServerConnector connector = new UnixDomainServerConnector(server, factories);
+    AFSocketServerConnector connector = new AFSocketServerConnector(server, factories);
+    String dir = System.getProperty("jetty.unixdomain.dir", System.getProperty("java.io.tmpdir"));
+    assertNotNull(dir);
+    File dirFile = new File(dir);
+    dirFile.mkdirs();
+    unixDomainPath = Files.createTempFile(dirFile.toPath(), "unix_", ".sock");
+    assertTrue(unixDomainPath.toAbsolutePath().toString().length() < 108,
+        "Unix-Domain path too long");
+    Files.delete(unixDomainPath);
+    // connector.setUnixDomainPath(unixDomainPath);
+    connector.setListenSocketAddress(AFUNIXSocketAddress.of(unixDomainPath));
+    server.addConnector(connector);
+    server.setHandler(handler);
+    server.start();
+  }
+
+  @AfterEach
+  public void dispose() {
+    LifeCycle.stop(server);
+  }
+
+  @Test
+  public void testHTTPOverUnixDomain() throws Exception {
+    String uri = "http://localhost:1234/path";
+    start(new Handler.Abstract() {
+
+      @Override
+      public boolean handle(Request request, Response response, Callback callback)
+          throws Exception {
+        // Verify the URI is preserved.
+        assertEquals(uri, request.getHttpURI().toString());
+
+        // Verify the SocketAddresses.
+        ConnectionMetaData connectionMetaData = request.getConnectionMetaData();
+        SocketAddress local = connectionMetaData.getLocalSocketAddress();
+        SocketAddress remote = connectionMetaData.getRemoteSocketAddress();
+
+        assertThat(local, Matchers.instanceOf(AFSocketAddress.class));
+        if (remote != null) {
+          // remote should be null if not connected
+          assertThat(remote, Matchers.instanceOf(AFSocketAddress.class));
+        }
+
+        assertDoesNotThrow(connectionMetaData::toString);
+        
+        response.write(true, ByteBuffer.allocate(0), new Callback() {});
+
+        return true;
+      }
+    });
+
+    // ClientConnector clientConnector = ClientConnector.forUnixDomain(unixDomainPath);
+    ClientConnector clientConnector = AFSocketClientConnector.withSocketAddress(AFUNIXSocketAddress
+        .of(unixDomainPath));
+    HttpClient httpClient = new HttpClient(new HttpClientTransportDynamic(clientConnector));
+    httpClient.start();
+    try {
+      ContentResponse response = httpClient.newRequest(uri).timeout(5, TimeUnit.SECONDS).send();
+
+      assertEquals(HttpStatus.OK_200, response.getStatus());
+    } finally {
+      httpClient.stop();
+    }
+  }
+
+  @Test
+  public void testHTTPOverUnixDomainWithHTTPProxy() throws Exception {
+    int fakeProxyPort = 4567;
+    int fakeServerPort = 5678;
+    start(new Handler.Abstract() {
+      @Override
+      public boolean handle(Request request, Response response, Callback callback)
+          throws Exception {
+        // Proxied requests must have an absolute URI.
+        HttpURI uri = request.getHttpURI();
+        assertNotNull(uri.getScheme());
+        assertEquals(fakeServerPort, uri.getPort());
+
+        response.write(true, ByteBuffer.allocate(0), new Callback() {});
+
+        return true;
+      }
+    });
+
+    // ClientConnector clientConnector = ClientConnector.forUnixDomain(unixDomainPath);
+    ClientConnector clientConnector = AFSocketClientConnector.withSocketAddress(AFUNIXSocketAddress
+        .of(unixDomainPath));
+
+    HttpClient httpClient = new HttpClient(new HttpClientTransportDynamic(clientConnector));
+    httpClient.getProxyConfiguration().addProxy(new HttpProxy("localhost", fakeProxyPort));
+    httpClient.start();
+    try {
+      ContentResponse response = httpClient.newRequest("localhost", fakeServerPort).timeout(5,
+          TimeUnit.SECONDS).send();
+
+      assertEquals(HttpStatus.OK_200, response.getStatus());
+    } finally {
+      httpClient.stop();
+    }
+  }
+
+  @Test
+  public void testHTTPOverUnixDomainWithProxyProtocol() throws Exception {
+    String srcAddr = "/proxySrcAddr";
+    String dstAddr = "/proxyDstAddr";
+    factories = new ConnectionFactory[] {new ProxyConnectionFactory(), new HttpConnectionFactory()};
+    start(new Handler.Abstract() {
+      @Override
+      public boolean handle(Request request, Response response, Callback callback)
+          throws Exception {
+        ConnectionMetaData connectionMetaData = request.getConnectionMetaData();
+        
+        String target = request.getHttpURI().getPath();
+        
+        if ("/v1".equals(target)) {
+          // As PROXYv1 does not support UNIX, the wrapped EndPoint data is used.
+          Path localPath = toUnixDomainPath(connectionMetaData.getLocalSocketAddress());
+          assertThat(localPath, Matchers.equalTo(unixDomainPath));
+        } else if ("/v2".equals(target)) {
+          SocketAddress localSocketAddress = connectionMetaData.getLocalSocketAddress();
+          if (localSocketAddress != null) {
+            assertThat(toUnixDomainPath(localSocketAddress).toString(), Matchers.equalTo(separators(
+                dstAddr)));
+          }
+          SocketAddress remoteSocketAddress = connectionMetaData.getRemoteSocketAddress();
+          if (remoteSocketAddress != null) {
+            assertThat(toUnixDomainPath(remoteSocketAddress).toString(), Matchers.equalTo(
+                separators(srcAddr)));
+          }
+        } else {
+          Assertions.fail("Invalid PROXY protocol version " + target);
+        }
+        
+        response.write(true, ByteBuffer.allocate(0), new Callback() {});
+
+        return true;
+      }
+    });
+
+    // Java 11+ portable way to implement SocketChannelWithAddress.Factory.
+    // ClientConnector clientConnector = ClientConnector.forUnixDomain(unixDomainPath);
+    ClientConnector clientConnector = AFSocketClientConnector.withSocketAddress(AFUNIXSocketAddress
+        .of(unixDomainPath));
+
+    HttpClient httpClient = new HttpClient(new HttpClientTransportDynamic(clientConnector));
+    httpClient.start();
+    try {
+      // Try PROXYv1 with the PROXY information retrieved from the EndPoint.
+      // PROXYv1 does not support the UNIX family.
+      ContentResponse response1 = httpClient.newRequest("localhost", 0).path("/v1").tag(
+          new V1.Tag()).timeout(5, TimeUnit.SECONDS).send();
+
+      assertEquals(HttpStatus.OK_200, response1.getStatus());
+
+      // Try PROXYv2 with explicit PROXY information.
+      V2.Tag tag = new V2.Tag(V2.Tag.Command.PROXY, V2.Tag.Family.UNIX, V2.Tag.Protocol.STREAM,
+          srcAddr, 0, dstAddr, 0, null);
+      ContentResponse response2 = httpClient.newRequest("localhost", 0).path("/v2").tag(tag)
+          .timeout(5, TimeUnit.SECONDS).send();
+
+      assertEquals(HttpStatus.OK_200, response2.getStatus());
+    } finally {
+      httpClient.stop();
+    }
+  }
+
+  @Test
+  public void testInvalidUnixDomainPath() {
+    server = new Server();
+    // UnixDomainServerConnector connector = new UnixDomainServerConnector(server, factories);
+    AFSocketServerConnector connector = new AFSocketServerConnector(server, factories);
+
+    // connector.setUnixDomainPath(new File("/does/not/exist").toPath());
+    try {
+      connector.setListenSocketAddress(AFUNIXSocketAddress.of(new File("/does/not/exist")
+          .toPath()));
+    } catch (SocketException e) {
+      throw new IllegalStateException(e);
+    }
+
+    server.addConnector(connector);
+    assertThrows(IOException.class, () -> server.start());
+  }
+
+  private static Path toUnixDomainPath(SocketAddress address) {
+    Objects.requireNonNull(address, "address");
+
+    if (address instanceof AFUNIXSocketAddress) {
+      return new File(((AFUNIXSocketAddress) address).getPath()).toPath();
+    } else if (unixDomainSocketAddressClass != null) {
+      try {
+        return (Path) unixDomainSocketAddressClass.getMethod("getPath").invoke(address);
+      } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException
+          | SecurityException e) {
+        Assertions.fail(e);
+        throw new AssertionError(e);
+      }
+    } else {
+      throw new IllegalStateException("Unsupported socket address class " + address.getClass()
+          + ": " + address);
+    }
+  }
+
+  public static String separators(String path) {
+    StringBuilder ret = new StringBuilder();
+    for (int i = 0, n = path.length(); i < n; i++) {
+      char c = path.charAt(i);
+      if ((c == '/') || (c == '\\')) {
+        ret.append(File.separatorChar);
+      } else {
+        ret.append(c);
+      }
+    }
+    return ret.toString();
+  }
+
+  @Test
+  public void testLargeBody() throws Exception {
+    String uri = "http://localhost:1234/path";
+
+    byte[] payload = new byte[512 * 1024]; // 512k
+    new Random().nextBytes(payload);
+
+    start(new Handler.Abstract() {
+      @Override
+      public boolean handle(Request request, Response response, Callback callback)
+          throws Exception {
+        response.getHeaders().put(HttpHeader.CONTENT_TYPE, "application/octet-stream");
+        
+        response.write(true, ByteBuffer.wrap(payload), new Callback() {});
+        
+        return true;
+      }
+    });
+
+    // ClientConnector clientConnector = ClientConnector.forUnixDomain(unixDomainPath);
+    ClientConnector clientConnector = AFSocketClientConnector.withSocketAddress(AFUNIXSocketAddress
+        .of(unixDomainPath));
+    HttpClient httpClient = new HttpClient(new HttpClientTransportDynamic(clientConnector));
+    httpClient.start();
+    try {
+      ContentResponse response = httpClient.newRequest(uri).timeout(5, TimeUnit.SECONDS).send();
+
+      assertEquals(HttpStatus.OK_200, response.getStatus());
+      byte[] data = response.getContent();
+      assertArrayEquals(payload, data);
+    } finally {
+      httpClient.stop();
+    }
+  }
+}
---
+++ junixsocket-jetty-12/src/test/java/org/newsclub/net/unix/jetty/AFSocketServerConnectorTest.java
@@ -0,0 +1,122 @@
+/*
+ * junixsocket
+ *
+ * Copyright 2009-2023 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.newsclub.net.unix.jetty;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.BufferedReader;
+import java.time.Duration;
+
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.Server;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.newsclub.net.unix.AFSocketAddress;
+import org.newsclub.net.unix.AFSocketFactory;
+import org.newsclub.net.unix.domain.AFUNIXAddressSpecifics;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+public class AFSocketServerConnectorTest {
+  @BeforeAll
+  public static void setUp() {
+    System.setProperty("org.slf4j.simpleLogger.log.org.eclipse.jetty.server", "error");
+  }
+
+  @Test
+  public void testServerAFUNIX() throws Exception {
+    AFSocketAddress addr = (AFSocketAddress) AFUNIXAddressSpecifics.INSTANCE
+        .newTempAddressForDatagram();
+    Server server1 = newServer(addr);
+    checkConnection(addr);
+    assertTrue(server1.isRunning());
+
+    Server server2 = newServer(addr);
+
+    // isDeleteOnClose is smart enough to not delete the wrong socket
+    assertTrue(addr.getFile().exists());
+
+    server1.join();
+    assertFalse(server1.isRunning());
+    server2.stop();
+    server2.join();
+    assertFalse(addr.getFile().exists()); // isDeleteOnClose=true by default
+  }
+
+  private void checkConnection(AFSocketAddress addr) throws Exception {
+    OkHttpClient.Builder builder = new OkHttpClient.Builder() //
+        .socketFactory(new AFSocketFactory.FixedAddressSocketFactory(addr)) //
+        .callTimeout(Duration.ofMinutes(1));
+
+    OkHttpClient client = builder.build();
+
+    Request request = new Request.Builder().url("http://localhost/").build();
+    try (Response response = client.newCall(request).execute()) {
+      assertEquals(404, response.code());
+      assertNotNull(response.header("Server"));
+
+      ResponseBody body = response.body();
+      assertNotNull(body);
+
+      BufferedReader br = new BufferedReader(body.charStream());
+      int l = 0;
+      while (br.readLine() != null) {
+        l++;
+      }
+      assertNotEquals(0, l);
+    }
+  }
+
+  private static Server newServer(AFSocketAddress addr) throws Exception {
+    Server server = new Server();
+
+    // below code is based upon
+    // https://www.eclipse.org/jetty/documentation/jetty-10/programming-guide/index.html
+
+    // The number of acceptor threads.
+    int acceptors = 1;
+
+    // The number of selectors.
+    int selectors = 1;
+
+    // Create a ServerConnector instance.
+    AFSocketServerConnector connector = new AFSocketServerConnector(server, acceptors, selectors,
+        new HttpConnectionFactory());
+
+    // The AFSocketAddress to listen to.
+    connector.setListenSocketAddress(addr);
+
+    // The accept queue size.
+    connector.setAcceptQueueSize(128);
+
+    // Try to automatically stop server if another instance reuses our address
+    connector.setMayStopServer(true);
+
+    server.addConnector(connector);
+    server.start();
+
+    return server;
+  }
+}
