af82a730cf9cd989772053601f51f16b27f2e6b3
Christian Kohlschütter <christian@kohlschutter.com>
2023-09-22 21:41:56 +0200

    ssl: Improve TestUtil

--- junixsocket-ssl/src/test/java/org/newsclub/net/unix/ssl/TestUtil.java
+++ junixsocket-ssl/src/test/java/org/newsclub/net/unix/ssl/TestUtil.java
@@ -19,25 +19,89 @@ package org.newsclub.net.unix.ssl;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
+import java.net.SocketException;
 import java.util.Arrays;
+import java.util.function.Supplier;
 
 final class TestUtil {
+
   private TestUtil() {
     throw new IllegalStateException("No instances");
   }
 
   @SafeVarargs
+  @SuppressWarnings("varargs")
   static void assertInstanceOf(Throwable t, Class<? extends Throwable>... expected) {
-    if (t == null) {
-      fail("Should have thrown something, specifically one of " + Arrays.asList(expected));
-      return;
-    }
-    Class<? extends Throwable> thrownClass = t.getClass();
+    Class<? extends Throwable> thrownClass = t == null ? null : t.getClass();
     for (Class<? extends Throwable> e : expected) {
-      if (e.isAssignableFrom(thrownClass)) {
+      if (thrownClass == null) {
+        if (e == null) {
+          return;
+        }
+      } else if (e == null) {
+        // continue
+      } else if (e.isAssignableFrom(thrownClass)) {
         return;
       }
     }
-    fail("Should have thrown one of " + Arrays.asList(expected) + " but did: " + t);
+
+    if (t == null) {
+      fail("Should have thrown something, specifically one of " + Arrays.toString(expected));
+    } else {
+      fail("Should have thrown one of " + Arrays.toString(expected) + " but did: " + t);
+    }
+  }
+
+  /**
+   * Handle the case where both server and client may throw exceptions, and where a
+   * {@link SocketException} is less interesting than something else.
+   *
+   * @param s1 The supplier of the first exception (preference given as tie breaker).
+   * @param s2 The supplier of the second exception
+   * @throws Exception on exception.
+   * @throws Error on error.
+   */
+  @SuppressWarnings("PMD.CognitiveComplexity")
+  static void throwMoreInterestingThrowableThanSocketException(Supplier<? extends Throwable> s1,
+      Supplier<? extends Throwable> s2) throws Exception {
+    Throwable t1 = s1.get();
+    Throwable t2 = s2.get();
+
+    // easy cases first
+    if (t1 == null) {
+      if (t2 == null) {
+        return;
+      } else {
+        if (t2 instanceof Error) {
+          throw (Error) t2;
+        } else {
+          throw (Exception) t2;
+        }
+      }
+    } else if (t2 == null) {
+      if (t1 instanceof Error) {
+        throw (Error) t1;
+      } else {
+        throw (Exception) t1;
+      }
+    }
+
+    if (t1 instanceof SocketException) {
+      if (t2 instanceof SocketException) {
+        throw (SocketException) t1;
+      } else {
+        if (t2 instanceof Error) {
+          throw (Error) t2;
+        } else {
+          throw (Exception) t2;
+        }
+      }
+    } else {
+      if (t1 instanceof Error) {
+        throw (Error) t1;
+      } else {
+        throw (Exception) t1;
+      }
+    }
   }
 }
