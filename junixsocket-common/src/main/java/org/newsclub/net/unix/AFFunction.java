6d46982fb30b2bcf4c583a0e45d8bd6059ae3300
Christian Kohlschütter <christian@kohlschutter.com>
2023-09-25 20:20:52 +0200

    Fix AFFunction, AFSupplier warnings

--- junixsocket-common/src/main/java/org/newsclub/net/unix/AFFunction.java
+++ junixsocket-common/src/main/java/org/newsclub/net/unix/AFFunction.java
@@ -1,7 +1,31 @@
+/*
+ * junixsocket
+ *
+ * Copyright 2009-2023 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.newsclub.net.unix;
 
+/**
+ * junixsocket-internal variant of {@code java.util.Function}, to allow compiling junixsocket-common
+ * with retrolambda for Java 1.7.
+ * 
+ * @param <T> the type of the input to the function
+ * @param <R> the type of the result of the function
+ */
 @FunctionalInterface
-public interface AFFunction<T, R> {
+interface AFFunction<T, R> {
 
   /**
    * Applies this function to the given argument.
