4cabdac09f47a47143e8e8d8868d2bb15a10bded
Christian Kohlschütter <christian@kohlschutter.com>
2023-09-26 16:57:36 +0200

    Add KnownJavaBugIOException
    Some errors we encounter truly come from bugs in the Java JRE/JDK.
    
    Selftest may report FAIL, even if it's not a defect in junixsocket.
    
    If our code (runtime or test) detect such a bug, wrap the IOException as
    KnownJavaBugIOException, specifying a descriptive message. test could
    should catch KnownJavaBugIOException where appropriate, throwing an
    TestAbortedWithImportantMessageException.

---
+++ junixsocket-common/src/main/java/org/newsclub/net/unix/KnownJavaBugIOException.java
@@ -0,0 +1,65 @@
+/*
+ * junixsocket
+ *
+ * Copyright 2009-2023 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.newsclub.net.unix;
+
+import java.io.IOException;
+
+/**
+ * Thrown when a known Java/JRE/JDK bug was encountered.
+ *
+ * @author Christian Kohlschütter
+ */
+public class KnownJavaBugIOException extends IOException {
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Constructs an {@code KnownJDKBugIOException} with {@code null} as its error detail message.
+   */
+  public KnownJavaBugIOException() {
+    super();
+  }
+
+  /**
+   * Constructs an {@code IOException} with the specified detail message and cause.
+   *
+   * @param message The message.
+   * @param cause The cause.
+   */
+  public KnownJavaBugIOException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  /**
+   * Constructs an {@code IOException} with the specified detail message.
+   *
+   * @param message The message.
+   */
+  public KnownJavaBugIOException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs an {@code IOException} with the specified cause and {@code null} as its error detail
+   * message.
+   *
+   * @param cause The cause.
+   */
+  public KnownJavaBugIOException(Throwable cause) {
+    super(cause);
+  }
+}
