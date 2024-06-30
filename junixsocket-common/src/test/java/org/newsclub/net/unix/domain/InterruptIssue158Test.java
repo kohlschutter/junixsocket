e7508d7c795ba4b99120cb34ec76196ad40af4c5
Christian Kohlschütter <christian@kohlschutter.com>
2024-06-30 19:28:33 +0200

    Reformat InterruptIssue158Test
    ... and add license header/Javadoc.
    
    https://github.com/kohlschutter/junixsocket/issues/158

--- junixsocket-common/src/test/java/org/newsclub/net/unix/domain/InterruptIssue158Test.java
+++ junixsocket-common/src/test/java/org/newsclub/net/unix/domain/InterruptIssue158Test.java
@@ -1,13 +1,25 @@
+/*
+ * junixsocket
+ *
+ * Copyright 2009-2024 Christian Kohlschütter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.newsclub.net.unix.domain;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.newsclub.net.unix.AFUNIXServerSocketChannel;
-import org.newsclub.net.unix.AFUNIXSocket;
-import org.newsclub.net.unix.AFUNIXSocketAddress;
-import org.newsclub.net.unix.AFUNIXSocketChannel;
-import org.newsclub.net.unix.AFUNIXServerSocket;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.net.SocketException;
@@ -28,274 +40,219 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Predicate;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.newsclub.net.unix.AFUNIXServerSocket;
+import org.newsclub.net.unix.AFUNIXServerSocketChannel;
+import org.newsclub.net.unix.AFUNIXSocket;
+import org.newsclub.net.unix.AFUNIXSocketAddress;
+import org.newsclub.net.unix.AFUNIXSocketChannel;
 
+/**
+ * Test interrupt-related behavior, as discussed in
+ * <a href="https://github.com/kohlschutter/junixsocket/issues/158">issue 158.</a>
+ * 
+ * @author https://github.com/cenodis
+ * @author Christian Kohlschütter
+ */
 public class InterruptIssue158Test {
 
-	private static final Path SOCKET_PATH = Path.of("/", "tmp", "test_socket");
-	private static final AFUNIXSocketAddress SOCKET_ADDR;
+  private static final Path SOCKET_PATH = Path.of("/", "tmp", "test_socket");
+  private static final AFUNIXSocketAddress SOCKET_ADDR;
 
-	static {
-		try {
-			SOCKET_ADDR = AFUNIXSocketAddress.of(SOCKET_PATH);
-		} catch (SocketException e) {
-			throw new RuntimeException(e);
-		}
-	}
+  static {
+    try {
+      SOCKET_ADDR = AFUNIXSocketAddress.of(SOCKET_PATH);
+    } catch (SocketException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-	private static List<Arguments> clientProvider() {
-		return List.of(
-			socket(false, AFUNIXSocket::newInstance, s -> s.connect(SOCKET_ADDR), SocketException.class, AFUNIXSocket::isClosed),
-			socket(true, () -> AFUNIXSocket.connectTo(SOCKET_ADDR), s -> s.getInputStream().read(), SocketException.class, AFUNIXSocket::isClosed),
-			socket(true, () -> AFUNIXSocket.connectTo(SOCKET_ADDR), s -> s.getOutputStream().write(10), SocketException.class, AFUNIXSocket::isClosed),
+  private static List<Arguments> clientProvider() {
+    return List.of(socket(false, AFUNIXSocket::newInstance, s -> s.connect(SOCKET_ADDR),
+        SocketException.class, AFUNIXSocket::isClosed), socket(true, () -> AFUNIXSocket.connectTo(
+            SOCKET_ADDR), s -> s.getInputStream().read(), SocketException.class,
+            AFUNIXSocket::isClosed), socket(true, () -> AFUNIXSocket.connectTo(SOCKET_ADDR), s -> s
+                .getOutputStream().write(10), SocketException.class, AFUNIXSocket::isClosed),
 
-			socket(false, AFUNIXSocketChannel::open, s -> s.connect(SOCKET_ADDR), ClosedByInterruptException.class, s -> !s.isOpen()),
-			socket(
-				true,
-				InterruptIssue158Test::connectSocketChannel,
-				s -> s.read(ByteBuffer.allocate(1)),
-				ClosedByInterruptException.class,
-				s -> !s.isOpen()
-			),
-			socket(
-				true,
-				InterruptIssue158Test::connectSocketChannel,
-				s -> s.write(ByteBuffer.allocate(1)),
-				ClosedByInterruptException.class,
-				s -> !s.isOpen()
-			)
-		);
-	}
+        socket(false, AFUNIXSocketChannel::open, s -> s.connect(SOCKET_ADDR),
+            ClosedByInterruptException.class, s -> !s.isOpen()), socket(true,
+                InterruptIssue158Test::connectSocketChannel, s -> s.read(ByteBuffer.allocate(1)),
+                ClosedByInterruptException.class, s -> !s.isOpen()), socket(true,
+                    InterruptIssue158Test::connectSocketChannel, s -> s.write(ByteBuffer.allocate(
+                        1)), ClosedByInterruptException.class, s -> !s.isOpen()));
+  }
 
-	private static List<Arguments> serverProvider() {
-		return List.of(
-			serverSocket(() -> AFUNIXServerSocket.bindOn(SOCKET_ADDR), AFUNIXServerSocket::accept, SocketException.class, AFUNIXServerSocket::isClosed),
-			serverSocket(InterruptIssue158Test::bindServerSocketChannel, AFUNIXServerSocketChannel::accept, ClosedByInterruptException.class, s -> !s.isOpen())
-		);
-	}
+  private static List<Arguments> serverProvider() {
+    return List.of(serverSocket(() -> AFUNIXServerSocket.bindOn(SOCKET_ADDR),
+        AFUNIXServerSocket::accept, SocketException.class, AFUNIXServerSocket::isClosed),
+        serverSocket(InterruptIssue158Test::bindServerSocketChannel,
+            AFUNIXServerSocketChannel::accept, ClosedByInterruptException.class, s -> !s.isOpen()));
+  }
 
+  @ParameterizedTest
+  @MethodSource("clientProvider")
+  <T extends AutoCloseable> void testClientInterruption(boolean acceptConnections,
+      IOSupplier<T> socket, IOConsumer<T> blockingOp, Class<?> expectedException,
+      Predicate<T> closeCheck) throws Throwable {
+    withServer(acceptConnections, () -> testSocketInterruption(false, socket, blockingOp,
+        expectedException, closeCheck));
+  }
 
-	@ParameterizedTest
-	@MethodSource("clientProvider")
-	<T extends AutoCloseable> void testClientInterruption(
-		boolean acceptConnections,
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) throws Throwable {
-		withServer(
-			acceptConnections,
-			() -> testSocketInterruption(false, socket, blockingOp, expectedException, closeCheck)
-		);
-	}
+  @ParameterizedTest
+  @MethodSource("clientProvider")
+  <T extends AutoCloseable> void testClientInterruptionWithDelay(boolean acceptConnections,
+      IOSupplier<T> socket, IOConsumer<T> blockingOp, Class<?> expectedException,
+      Predicate<T> closeCheck) throws Throwable {
+    withServer(acceptConnections, () -> testSocketInterruption(true, socket, blockingOp,
+        expectedException, closeCheck));
+  }
 
-	@ParameterizedTest
-	@MethodSource("clientProvider")
-	<T extends AutoCloseable> void testClientInterruptionWithDelay(
-		boolean acceptConnections,
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) throws Throwable {
-		withServer(
-			acceptConnections,
-			() -> testSocketInterruption(true, socket, blockingOp, expectedException, closeCheck)
-		);
-	}
+  @ParameterizedTest
+  @MethodSource("serverProvider")
+  <T extends AutoCloseable> void testServerInterruption(IOSupplier<T> socket,
+      IOConsumer<T> blockingOp, Class<?> expectedException, Predicate<T> closeCheck)
+      throws Throwable {
+    try {
+      testSocketInterruption(false, socket, blockingOp, expectedException, closeCheck);
+    } finally {
+      Files.deleteIfExists(SOCKET_PATH);
+    }
+  }
 
-	@ParameterizedTest
-	@MethodSource("serverProvider")
-	<T extends AutoCloseable> void testServerInterruption(
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) throws Throwable {
-		try {
-			testSocketInterruption(
-				false,
-				socket,
-				blockingOp,
-				expectedException,
-				closeCheck
-			);
-		} finally {
-			Files.deleteIfExists(SOCKET_PATH);
-		}
-	}
+  @ParameterizedTest
+  @MethodSource("serverProvider")
+  <T extends AutoCloseable> void testServerInterruptionWithDelay(IOSupplier<T> socket,
+      IOConsumer<T> blockingOp, Class<?> expectedException, Predicate<T> closeCheck)
+      throws Throwable {
+    try {
+      testSocketInterruption(true, socket, blockingOp, expectedException, closeCheck);
+    } finally {
+      Files.deleteIfExists(SOCKET_PATH);
+    }
+  }
 
-	@ParameterizedTest
-	@MethodSource("serverProvider")
-	<T extends AutoCloseable> void testServerInterruptionWithDelay(
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) throws Throwable {
-		try {
-			testSocketInterruption(
-				true,
-				socket,
-				blockingOp,
-				expectedException,
-				closeCheck
-			);
-		} finally {
-			Files.deleteIfExists(SOCKET_PATH);
-		}
-	}
+  <T extends AutoCloseable> void testSocketInterruption(boolean delay, IOSupplier<T> socket,
+      IOConsumer<T> blockingOp, Class<?> expectedException, Predicate<T> closeCheck)
+      throws Throwable {
+    var exceptionHolder = new AtomicReference<Throwable>();
+    var ready = new CountDownLatch(1);
+    var t = Thread.ofVirtual().start(() -> exceptionHolder.set(runOperation(ready, socket,
+        blockingOp, expectedException, closeCheck)));
 
+    ready.await();
+    if (delay) {
+      Thread.sleep(500);
+    }
+    t.interrupt();
+    if (!t.join(Duration.of(1, ChronoUnit.SECONDS))) {
+      throw new RuntimeException("Thread failed to terminate after interrupt");
+    }
+    var thrownException = exceptionHolder.get();
+    if (thrownException != null) {
+      throw thrownException;
+    }
+  }
 
-	<T extends AutoCloseable> void testSocketInterruption(
-		boolean delay,
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) throws Throwable {
-		var exceptionHolder = new AtomicReference<Throwable>();
-		var ready = new CountDownLatch(1);
-		var t = Thread.ofVirtual()
-			.start(() -> exceptionHolder.set(runOperation(
-				ready,
-				socket,
-				blockingOp,
-				expectedException,
-				closeCheck
-			)));
+  private static void withServer(boolean acceptConnections, ThrowingRunnable func)
+      throws Throwable {
+    try (var serverSocket = ServerSocketChannel.open(StandardProtocolFamily.UNIX)) {
+      serverSocket.bind(UnixDomainSocketAddress.of(SOCKET_PATH));
+      Thread serverThread = null;
+      if (acceptConnections) {
+        serverThread = Thread.ofPlatform().daemon(true).start(() -> {
+          var clients = new ArrayList<SocketChannel>();
+          while (serverSocket.isOpen()) {
+            try {
+              var socket = serverSocket.accept();
+              clients.add(socket);
+            } catch (ClosedChannelException e) {
+              return;
+            } catch (IOException e) {
+              throw new RuntimeException("Unable to accept socket ", e);
+            } finally {
+              for (var client : clients) {
+                try {
+                  client.close();
+                } catch (IOException ignored) {
+                }
+              }
+            }
+          }
+        });
+      }
+      try {
+        func.run();
+      } finally {
+        serverSocket.close();
+        if (serverThread != null) {
+          serverThread.join();
+        }
+      }
+    } finally {
+      Files.deleteIfExists(SOCKET_PATH);
+    }
+  }
 
-		ready.await();
-		if (delay) {
-			Thread.sleep(500);
-		}
-		t.interrupt();
-		if (!t.join(Duration.of(1, ChronoUnit.SECONDS))) {
-			throw new RuntimeException("Thread failed to terminate after interrupt");
-		}
-		var thrownException = exceptionHolder.get();
-		if (thrownException != null) {
-			throw thrownException;
-		}
-	}
+  <T extends AutoCloseable> Throwable runOperation(CountDownLatch ready, IOSupplier<T> socket,
+      IOConsumer<T> blockingOp, Class<?> expectedException, Predicate<T> closeCheck) {
+    try {
+      var sock = socket.get();
+      ready.countDown();
+      try {
+        blockingOp.accept(sock);
+      } catch (Exception e) {
+        assertAll(() -> assertInstanceOf(expectedException, e, "Socket exception"),
+            () -> assertTrue(Thread.interrupted(), "Thread interrupted"), () -> assertTrue(
+                closeCheck.test(sock), "Socket closed"));
+      } finally {
+        ready.countDown();
+        if (sock != null) {
+          try {
+            sock.close();
+          } catch (Exception e) {
+            throw new RuntimeException("Unable to clean up socket", e);
+          }
+        }
+      }
+    } catch (Throwable e) {
+      return e;
+    }
+    return null;
+  }
 
-	private static void withServer(boolean acceptConnections, ThrowingRunnable func) throws Throwable {
-		try(var serverSocket = ServerSocketChannel.open(StandardProtocolFamily.UNIX)) {
-			serverSocket.bind(UnixDomainSocketAddress.of(SOCKET_PATH));
-			Thread serverThread = null;
-			if (acceptConnections) {
-				serverThread = Thread.ofPlatform()
-					.daemon(true)
-					.start(() -> {
-						var clients = new ArrayList<SocketChannel>();
-						while (serverSocket.isOpen()) {
-							try {
-								var socket = serverSocket.accept();
-								clients.add(socket);
-							} catch (ClosedChannelException e) {
-								return;
-							} catch (IOException e) {
-								throw new RuntimeException("Unable to accept socket ", e);
-							} finally {
-								for (var client : clients) {
-									try {
-										client.close();
-									} catch (IOException ignored) {
-									}
-								}
-							}
-						}
-					});
-			}
-			try {
-				func.run();
-			} finally {
-				serverSocket.close();
-				if (serverThread != null) {
-					serverThread.join();
-				}
-			}
-		} finally {
-			Files.deleteIfExists(SOCKET_PATH);
-		}
-	}
+  private static <T> Arguments socket(boolean acceptConnections, IOSupplier<T> socket,
+      IOConsumer<T> blockingOp, Class<?> expectedException, Predicate<T> closeCheck) {
+    return Arguments.of(acceptConnections, socket, blockingOp, expectedException, closeCheck);
+  }
 
-	<T extends AutoCloseable> Throwable runOperation(
-		CountDownLatch ready,
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) {
-		try {
-			var sock = socket.get();
-			ready.countDown();
-			try {
-				blockingOp.accept(sock);
-			} catch (Exception e) {
-				assertAll(
-					() -> assertInstanceOf(expectedException, e, "Socket exception"),
-					() -> assertTrue(Thread.interrupted(), "Thread interrupted"),
-					() -> assertTrue(closeCheck.test(sock), "Socket closed")
-				);
-			} finally {
-				ready.countDown();
-				if (sock != null) {
-					try {
-						sock.close();
-					} catch (Exception e) {
-						throw new RuntimeException("Unable to clean up socket", e);
-					}
-				}
-			}
-		} catch (Throwable e) {
-			return e;
-		}
-		return null;
-	}
+  private static <T> Arguments serverSocket(IOSupplier<T> socket, IOConsumer<T> blockingOp,
+      Class<?> expectedException, Predicate<T> closeCheck) {
+    return Arguments.of(socket, blockingOp, expectedException, closeCheck);
+  }
 
+  private static AFUNIXSocketChannel connectSocketChannel() throws IOException {
+    var socket = AFUNIXSocketChannel.open();
+    socket.connect(SOCKET_ADDR);
+    return socket;
+  }
 
-	private static <T> Arguments socket(
-		boolean acceptConnections,
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) {
-		return Arguments.of(acceptConnections, socket, blockingOp, expectedException, closeCheck);
-	}
+  private static AFUNIXServerSocketChannel bindServerSocketChannel() throws IOException {
+    var socket = AFUNIXServerSocketChannel.open();
+    socket.bind(SOCKET_ADDR);
+    return socket;
+  }
 
-	private static <T> Arguments serverSocket(
-		IOSupplier<T> socket,
-		IOConsumer<T> blockingOp,
-		Class<?> expectedException,
-		Predicate<T> closeCheck
-	) {
-		return Arguments.of(socket, blockingOp, expectedException, closeCheck);
-	}
+  private interface IOSupplier<T> {
+    T get() throws IOException;
+  }
 
-	private static AFUNIXSocketChannel connectSocketChannel() throws IOException {
-		var socket = AFUNIXSocketChannel.open();
-		socket.connect(SOCKET_ADDR);
-		return socket;
-	}
+  private interface IOConsumer<T> {
+    void accept(T t) throws IOException;
+  }
 
-	private static AFUNIXServerSocketChannel bindServerSocketChannel() throws IOException {
-		var socket = AFUNIXServerSocketChannel.open();
-		socket.bind(SOCKET_ADDR);
-		return socket;
-	}
-
-	private interface IOSupplier<T> {
-		T get() throws IOException;
-	}
-
-	private interface IOConsumer<T> {
-		void accept(T t) throws IOException;
-	}
-
-	private interface ThrowingRunnable {
-		void run() throws Throwable;
-	}
+  private interface ThrowingRunnable {
+    void run() throws Throwable;
+  }
 }
