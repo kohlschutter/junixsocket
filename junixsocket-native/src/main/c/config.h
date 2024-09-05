3d4553e96ca8f4499a7ab643fa4838c429fa2914
Christian Kohlschütter <christian@kohlschutter.com>
2024-09-05 02:18:11 +0200

    native: Don't use __xstat on loongarch64
    We currently define __xstat to allow linking against older glibc builds.
    
    glibc on loongarch64 does not have __xstat (not necessary), which
    currently breaks our native library.
    
    Detect the loongarch architecture and skip our xstat workaround.
    
    Also see https://lists.openembedded.org/g/openembedded-core/topic/99216091

--- junixsocket-native/src/main/c/config.h
+++ junixsocket-native/src/main/c/config.h
@@ -367,7 +367,10 @@ int jux_mangleErrno(int);
 #  else
 // This allows us to link against older glibc versions
 #    define memcpy memmove
-#    ifndef _STAT_VER
+#    if defined(__loongarch64)
+// no stat/__xstat workaround on loongarch64
+#    else
+#      ifndef _STAT_VER
 #        if defined(__aarch64__) || defined(__riscv)
 #            define _STAT_VER 0
 #        elif defined(__x86_64__)
@@ -375,12 +378,13 @@ int jux_mangleErrno(int);
 #        else
 #            define _STAT_VER 3
 #        endif
-#    endif
-#    if !defined(__xstat)
+#      endif
+#      if !defined(__xstat)
 extern int __xstat (int __ver, const char *__filename,
                     struct stat *__stat_buf) __THROW __nonnull ((2, 3));
+#      endif
+#      define stat(...) __xstat(_STAT_VER, __VA_ARGS__)
 #    endif
-#    define stat(...) __xstat(_STAT_VER, __VA_ARGS__)
 #  endif
 #endif
 
