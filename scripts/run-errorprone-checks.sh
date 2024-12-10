d8a0249e112dcfeb40b9d2c4be4349c8d7562ccf
Christian Kohlschütter <christian@kohlschutter.com>
2024-12-10 14:30:38 +0100

    scripts: errorprone: Show errors, return code at the end

--- scripts/run-errorprone-checks.sh
+++ scripts/run-errorprone-checks.sh
@@ -19,6 +19,7 @@ fi
 
 out=()
 
+hasError=0
 while read l; do
 	#echo $l;
 	case "$l" in
@@ -32,10 +33,18 @@ while read l; do
 			;;
 		"--")
 			;;
+		"[ERROR]"*)
+			hasError=1
+			echo $l
+			;;
 		*)
 			echo $l
 			;;
 	esac
 done < <(
-MAVEN_OPTS="--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" mvn clean compile ${opts[@]} -Derrorprone -P \!multirelease,\!multirelease-java9,\!multirelease-java10,\!multirelease-java11,\!multirelease-java12,\!multirelease-java13,\!multirelease-java14,\!multirelease-java15,\!multirelease-java16,\!multirelease-java17,\!multirelease-java18,\!multirelease-java19,\!multirelease-java20,\!multirelease-java21,\!multirelease-java22 | grep -C2 errorprone.info
+set +e
+MAVEN_OPTS="--add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.main=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.processing=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.comp=ALL-UNNAMED" mvn clean compile ${opts[@]} -Derrorprone -P \!multirelease,\!multirelease-java9,\!multirelease-java10,\!multirelease-java11,\!multirelease-java12,\!multirelease-java13,\!multirelease-java14,\!multirelease-java15,\!multirelease-java16,\!multirelease-java17,\!multirelease-java18,\!multirelease-java19,\!multirelease-java20,\!multirelease-java21,\!multirelease-java22 | grep -i -C2 error
+#prone.info
 )
+echo errorprone RC=$hasError
+exit $hasError
